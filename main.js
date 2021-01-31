f9edf970ee9950b7c26bc6a22787eb4c0b7e1033
neon <neon_dust@systemli.org>
2021-01-31 16:31:17 +0100

    add some dishes to all the dish arrays

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const baseIngredient = {
-    rice: [],
-    pasta: [],
-    couscous: []
+    rice: ['Curry', 'Risotto', 'Kimchi Fried Rice', 'Rice Salad', 'Paella', 'Chili con Tofu', 'Teriyaki Bowl', 'Bibimbab', 'Tofu Yasai Don', 'Maki'],
+    pasta: ['Spaghetti with Tomato Sauce', 'Rice Noodle Salad', 'Ramen', 'Pasta with Zucchini Cream Sauce', 'Udon Noodle Soup', 'Miso Noodle Salad', 'Yakisoba', 'Pad Thai', 'Soba Salad', 'Pho']
 };
 
 const additionalDish = {
-    sideDish: [],
-    dessert: []
+    sideDish: ['Wakame Salad', 'Gyoza', 'Cucumber Salad', 'Salad', 'Kimchi', 'Bruschetta', 'grilled Artichokes'],
+    dessert: ['Mochi', 'Pudding', 'Red Berry Compot', 'Crepe', 'Icecream', 'Cookie', 'Mitarashi Dango', 'Waffles', 'Nama Chocolate']
 };
